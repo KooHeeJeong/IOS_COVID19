4c300d6f130af9ce5e19ccad3f0dc8f14a401a32
koohee <ko_ohee@naver.com>
2022-03-13 18:20:47 +0900

    cocoaPods library - chart, Alamofire add and UI add

---
+++ COVID19/CovidDetailViewController.swift
@@ -0,0 +1,34 @@
+//
+//  CovidDetailViewController.swift
+//  COVID19
+//
+//  Created by 구희정 on 2022/03/13.
+//
+
+import UIKit
+
+class CovidDetailViewController: UITableViewController {
+
+    @IBOutlet weak var newCaseCell: UITableViewCell!
+    @IBOutlet weak var totalCaseCell: UITableViewCell!
+    @IBOutlet weak var recoverdCell: UITableViewCell!
+    @IBOutlet weak var deathCell: UITableViewCell!
+    @IBOutlet weak var percentageCell: UITableViewCell!
+    @IBOutlet weak var overseasInflowCell: UITableViewCell!
+    @IBOutlet weak var regionalOutbreakCell: UITableViewCell!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 0
+    }
+
+}
